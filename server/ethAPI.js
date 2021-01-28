91e450d8ac5c493ba26d1b4751cc3fb95a676204
aayush <aayushg@mit.edu>
2021-01-27 21:26:19 -0800

    figured out ethabi

--- server/ethAPI.js
+++ server/ethAPI.js
@@ -1,17 +1,13 @@
 const { Contract, ContractFactory, providers } = require("ethers");
 const fetch = require("node-fetch");
-
+const fs = require("fs");
 const provider = new providers.JsonRpcProvider("http://localhost:8545");
 const signer = provider.getSigner();
 const contract = connect();
 
-const contractDir = "/home/cyril/aayush/ethereum/zk-polling/";
-
 async function connect(contractName) {
-  console.log(__dirname + "/../contracts/json/CoreValidator.json");
-  const contractJSON = await fetch(
-    contractDir + "/contracts/json/CoreValidator.json"
-  ).then((x) => x.json());
+  const location = __dirname + "/../contracts/json/CoreValidator.json";
+  const contractJSON = JSON.parse(fs.readFileSync(location)); // .json
   const contractABI = contractJSON.abi;
   const contractAddress = contractJSON.address;
 

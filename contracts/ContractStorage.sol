64f1c86004bf4b21ac5a2cd594f43d719e53b386
aayush <aayushg@mit.edu>
2021-01-28 20:11:22 -0800

    updated contract db schema and added getters and setters

--- contracts/ContractStorage.sol
+++ contracts/ContractStorage.sol
@@ -6,7 +6,35 @@ contract ContractStorage {
   //////////////
   ///  VARS  ///
   //////////////
-  mapping(bytes32 => bool) public hashIsVerified; // public auto assigns a getter
-  mapping(string => uint256[]) public registeredHashes; // public auto assigns a getter
+  uint256 public constant MAX_USERS = 10;
+
+  // maps generated message ids to group names
+  struct Message {
+    string text;
+    bool verified;
+  }
+  // Maps contiguous confession ids to messages
+  Message[] public confessions;
+  uint256 confessionCount;
+
+  // maps group names to hash of public keys
+  struct Group {
+    uint256 passwordHash;
+    uint256[MAX_USERS] users;
+    uint256 userCount;
+  }
+
+  // maps names to consecutive group IDs < groupCount
+  mapping(string => uint256) public groupIDs;
+
+  // maps each of the groupCount groups to the info
+  Group[10] groups;
+  mapping(string => bool) public groupExists;
+
+  // We have already assigned groups [0, 1...groupCount)
+  // We next assign the incoming group to groupCount
+  // Note that we dont need id's anymore since its sequential
+  uint256 groupCount;
+
   uint256 public pfsVerified;
 }
